bc99b26eee6f22d8f5bdc4b9aa9a47a3606c176d
HONGRUNYU <michaelhong1995@gmail.com>
2020-06-24 15:48:57 -0400

    6.24.1

---
+++ scripts/Mitochondria.R
@@ -0,0 +1,11 @@
+# Mitochondria
+# Data preparation
+library(readxl)
+library(biomaRt)
+library(org.Hs.eg.db)
+library(stringr)
+library(readr)
+library(plyr)
+library(ComplexHeatmap)
+library(RColorBrewer)
+library(circlize)
